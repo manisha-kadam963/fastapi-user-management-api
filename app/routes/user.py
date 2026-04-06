3006a974e9ade7b83152213ab4c0cfb3480150c9
Manisha Kadam <kadammanisha385@gmail.com>
2026-04-06 22:11:39 +0530

    Add Crud get method

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,8 +1,39 @@
 from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.database import SessionLocal
+from app import models
 from app.dependencies import get_current_user
 
 router = APIRouter()
 
+# DB dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+#  Existing code 
 @router.get("/profile")
 def get_profile(user=Depends(get_current_user)):
     return {"user": user}
+
+
+# NEW CODE CRUD get method
+
+@router.get("/users")
+def get_users(db: Session = Depends(get_db)):
+    users = db.query(models.User).all()
+    return users
+
+
+@router.get("/users/{user_id}")
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+
+    if not user:
+        return {"error": "User not found"}
+
+    return user
